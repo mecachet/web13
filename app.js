a76f37b4f197ee9e3e768a7f8ec6f3753792a254
mecachet <otoqvilitaia@gmail.com>
2023-07-15 01:25:43 +0400

    Added code to JS

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log("JavaScript file is linked correctly. Hooray.");
+
+let myNumber = 22;
+console.log(myNumber);
+console.log(typeof myNumber);
+
+var firstName = "Otar";
+var lastName = "Qvilitaia";
+var fullName = firstName + " " + lastName
+
+let myBoolean = false;
+console.log(myBoolean);
+console.log(typeof myBoolean);
+
+let myUndefined;
+console.log(myUndefined);
+console.log(typeof myUndefined);
+
+let myNull = null;
+console.log(myNull);
+console.log(typeof myNull);
+
+let myName = "Otar";
+let myHobby = "fishkeeping";
+let yearOfBirth = 2001;
+let currentYear = 2023;
+let myAge = currentYear - yearOfBirth;
+
+console.log(
+  `My name is ${myName}, I am currently ${
+    currentYear - yearOfBirth
+  } years old and my hobby is ${myHobby}.`
+);
